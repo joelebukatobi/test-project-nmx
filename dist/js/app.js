a794e0c44119bec191cfb07156a8b8da47adeda4
Joel Onwuanaku <joelebuka@gmail.com>
2023-11-04 04:05:28 -0400

    Initial Commit

--- dist/js/app.js
+++ dist/js/app.js
@@ -31,19 +31,19 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 
-  // Array of image links corresponding to each customer
-  const customerAvatar = [
-    'https://images.unsplash.com/photo-1597071058713-6776461d7737',
-    'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d',
-    'https://images.unsplash.com/photo-1557053908-4793c484d06f',
-    'https://images.unsplash.com/photo-1607503873903-c5e95f80d7b9',
-    'https://images.unsplash.com/photo-1554727242-741c14fa561c',
-    'https://images.unsplash.com/photo-1494790108377-be9c29b29330',
-    'https://plus.unsplash.com/premium_photo-1675129779554-dc86569708c8',
-    'https://images.unsplash.com/photo-1590086782792-42dd2350140d',
-    'https://images.unsplash.com/photo-1609505848912-b7c3b8b4beda',
-    'https://images.unsplash.com/photo-1491349174775-aaafddd81942',
-  ];
+  // Object of image links corresponding to each customer's id
+  const customerAvatar = {
+    1: 'https://images.unsplash.com/photo-1597071058713-6776461d7737',
+    2: 'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d',
+    3: 'https://images.unsplash.com/photo-1557053908-4793c484d06f',
+    4: 'https://images.unsplash.com/photo-1607503873903-c5e95f80d7b9',
+    5: 'https://images.unsplash.com/photo-1554727242-741c14fa561c',
+    6: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330',
+    7: 'https://plus.unsplash.com/premium_photo-1675129779554-dc86569708c8',
+    8: 'https://images.unsplash.com/photo-1590086782792-42dd2350140d',
+    9: 'https://images.unsplash.com/photo-1609505848912-b7c3b8b4beda',
+    10: 'https://images.unsplash.com/photo-1491349174775-aaafddd81942',
+  };
 
   // Function to fetch and display customers based on sortOrder and searchKeyword
   function fetchAndDisplayCustomers(sortOrder, searchKeyword) {
@@ -70,13 +70,13 @@ document.addEventListener('DOMContentLoaded', function () {
         // Clear the customerList
         customerList.innerHTML = '';
 
-        data.forEach((customer, index) => {
+        data.forEach((customer) => {
           const article = document.createElement('article');
           article.innerHTML = `
           
             <section>  
             <div class="picture">
-            <img src="${customerAvatar[index]}" alt="${customer.name}'s Picture">
+            <img src="${customerAvatar[customer.id]}" alt="${customer.name}'s Picture">
             </div>
             <header>
                 <h4>${customer.name}</h4>
